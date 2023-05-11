b3ffb6d6b8b65b88c6efcb626e61153036e9f061
jessy <jessy>
2023-05-11 22:52:53 +0800

    [*]提交一版

--- setup.py
+++ setup.py
@@ -9,28 +9,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='weblate-chatgpt',
-    version='0.1.0',
-    description='ChatGPT machine translation engine for Weblate',
-    author='Dennis',
-    author_email='dingxing@xd.com',
-    url='https://github.com/your_username/weblate-chatgpt',
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        'requests',
-        'openai'
-    ],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ],
+    name="weblate_chatgpt",
+    version="0.0.1",
+    author="Your name",
+    author_email="yourname@example.com",
+    description="ChatGPT plugin for Weblate",
+    license="GPLv3+",
+    keywords="Weblate ChatGPT plugin",
+    packages=["weblate_chatgpt"],
 )
